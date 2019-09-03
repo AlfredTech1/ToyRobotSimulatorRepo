52a44b451e8bdbe162981d500556b2042a07d56e
AlfredTech1 <54845063+AlfredTech1@users.noreply.github.com>
2019-09-03 22:08:55 +1000

    Add files via upload

---
+++ ToyRobotSimulator.BusinessLayer/Interfaces/IToyRobot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator.BusinessLayer.Interfaces
+{
+    public interface IToyRobot
+    {
+        // location of toy robot in the table surface.
+        Location Location { get; set; }
+
+        // Execute toy robot action on the table surface.
+        string DoCommand(string input);
+
+    }
+}
